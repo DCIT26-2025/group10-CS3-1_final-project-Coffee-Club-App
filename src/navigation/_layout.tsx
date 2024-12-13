f5509127582d1a83dc767b7eea444f26dd7331f7
javarice_plusplus <adrianmagsino2004@gmail.com>
2024-12-14 00:48:08 +0800

    added more screens

--- src/navigation/_layout.tsx
+++ src/navigation/_layout.tsx
@@ -4,6 +4,8 @@ import { Stack } from 'expo-router'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from '../screens/HomeScreen'
 import SettingsScreen from '../screens/SettingsScreen'
+import CartScreen from '../screens/CartScreen'
+import OrderStatusScreen from '../screens/OrderStatusScreen'
 
 const BottomTabNavigator = createBottomTabNavigator();
 const TabsLayout = () => {
@@ -17,7 +19,22 @@ const TabsLayout = () => {
             headerTitle: "The Coffee Club"
         }}
         />
-        <BottomTabNavigator.Screen name="Settings" component={SettingsScreen} />
+        <BottomTabNavigator.Screen 
+        name="CartScreen" 
+        component={CartScreen} 
+        options={{
+          tabBarLabel: "Cart",
+          headerTitle: "Cart"
+        }}
+        />
+        <BottomTabNavigator.Screen 
+        name="OrderStatusScreen" 
+        component={OrderStatusScreen} 
+        options={{
+          tabBarLabel: "Order",
+          headerTitle: "Your Orders"
+        }}
+        />
     </BottomTabNavigator.Navigator>
   )
 }
---
+++ src/screens/OrderStatusScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OrderStatusScreen = () => {
+  return (
+    <View>
+      <Text>OrderStatusScreen</Text>
+    </View>
+  )
+}
+
+export default OrderStatusScreen
