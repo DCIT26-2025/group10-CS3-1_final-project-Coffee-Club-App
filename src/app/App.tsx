a78116c7fbe3801f917a9d7a398f9490b8cff7a8
Jomel <renzen.miracle@gmail.com>
2025-01-24 02:46:49 +0800

    supabase login stuff

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import TabsLayout from '../navigation/_layout';
-import { NavigationContainer } from '@react-navigation/native';
-
+import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import TabsLayout from "../navigation/_layout";
+import { NavigationContainer } from "@react-navigation/native";
+import { supabase } from "../lib/supabase";
+import Auth from "../screens/LoginScreen";
+import { Session } from "@supabase/supabase-js";
 
 export default function App() {
-  return (
-    <TabsLayout />
-  )
-}
+  const [session, setSession] = useState<Session | null>(null);
 
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return <View>{session && session.user ? <TabsLayout /> : <Auth />}</View>;
+}
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,51 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  Alert,
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
   Image,
+  AppState,
 } from "react-native";
+import { supabase } from "../lib/supabase";
+import { Button, Input } from "@rneui/themed";
 
-export default function AuthScreen() {
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
+
+export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function signInWithEmail() {
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+  }
+
+  async function signUpWithEmail() {
+    setLoading(true);
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+    if (error) Alert.alert(error.message);
+    if (!session)
+      Alert.alert("Please check your inbox for email verification!");
+    setLoading(false);
+  }
   return (
     <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image
-          source={require("./assets/TCC_Logo.png")}
-          style={styles.logo}
-          resizeMode="contain"
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Input
+          label="Email"
+          leftIcon={{ type: "font-awesome", name: "envelope" }}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          placeholder="email@address.com"
+          autoCapitalize={"none"}
         />
       </View>
-      <Text style={styles.title}>the coffee club.</Text>
-
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Contact number</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="phone-pad"
-          placeholder="Enter contact number"
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="Password"
+          leftIcon={{ type: "font-awesome", name: "lock" }}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize={"none"}
         />
-
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          style={styles.input}
-          secureTextEntry
-          placeholder="Enter password"
-        />
-
-        <Text style={styles.forgotPassword}>Forgot Password</Text>
       </View>
-
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.signUpButton}>
-          <Text style={styles.signUpText}>Sign Up</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.loginButton}>
-          <Text style={styles.loginText}>Log In</Text>
-        </TouchableOpacity>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Button
+          title="Sign in"
+          disabled={loading}
+          onPress={() => signInWithEmail()}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Button
+          title="Sign up"
+          disabled={loading}
+          onPress={() => signUpWithEmail()}
+        />
       </View>
     </View>
   );
@@ -53,91 +94,15 @@ export default function AuthScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F8F5F1",
+    marginTop: 40,
+    padding: 12,
   },
-  logoContainer: {
-    width: 120,
-    height: 120,
-    marginBottom: 20,
-    justifyContent: "center",
-    alignItems: "center",
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: "stretch",
   },
-  logo: {
-    width: "100%",
-    height: "100%",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#5B4328",
-    marginBottom: 30,
-  },
-  inputContainer: {
-    width: "80%",
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: "bold",
-    marginBottom: 5,
-    color: "#5B4328",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#B99972",
-    borderRadius: 8,
-    padding: 10,
-    marginBottom: 15,
-    fontSize: 16,
-    color: "#333",
-  },
-  forgotPassword: {
-    color: "#5B4328",
-    textAlign: "right",
-    marginBottom: 15,
-    textDecorationLine: "underline",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "80%",
-    marginTop: 10,
-  },
-  signUpButton: {
-    flex: 1,
-    marginRight: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: "#5B4328",
-    borderRadius: 8,
-    alignItems: "center",
-  },
-  signUpText: {
-    color: "#5B4328",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  loginButton: {
-    flex: 1,
-    marginLeft: 10,
-    backgroundColor: "#B99972",
-    padding: 10,
-    borderRadius: 8,
-    alignItems: "center",
-  },
-  loginText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
+  mt20: {
+    marginTop: 20,
   },
 });
